4cc487c01a3ea9a248578e3c9705f5b56b15326b
Pratik Giri <pratikgiri043@gmail.com>
2025-10-04 21:13:04 +0530

    added cusotm header example

---
+++ custom_header_small_example/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "add.h"
+
+void main(void){
+    float first_number = 20.221;
+    float last_number = 50.21;
+
+    printf("This result is %f\n", add(first_number, last_number));
+}
